996326dd665ede53023d72a0b7a23ef7eacd95c6
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-04-05 19:15:22 +0200

    Frontend Mentor - Single Price Grid

--- single-price-grid/src/components/Left.jsx
+++ single-price-grid/src/components/Left.jsx
@@ -6,10 +6,10 @@ export const Left = () => {
         <div className={classes['container']}>
             <h3>Monthly Subscription</h3>
             <div className={classes['container-price']}>
-                <p><span className={classes['price']}>$29</span></p>
+                <p><span className={classes['price']}>&#36;29</span></p>
                 <p className={classes['time']}>per month</p>
             </div>
-            <p className={classes['info']}>Full access for less than $1 a day</p>
+            <p className={classes['info']}>Full access for less than &#36;1 a day</p>
             <button>Sign Up</button>
         </div>
     )
--- single-price-grid/src/components/Right.jsx
+++ single-price-grid/src/components/Right.jsx
@@ -3,6 +3,17 @@ import classes from './Right.module.css';
 
 export const Right = () => {
     return (
-        <div className={classes['container']}>Right</div>
+        <div className={classes['container']}>
+            <h3>Why Us</h3>
+            <p>
+                Tutorials by industry experts <br />
+                Peer &amp; expert code review <br />
+                Coding exercises <br />
+                Access to our GitHub repos <br />
+                Community forum <br />
+                Flashcard decks <br />
+                New videos every week
+            </p>
+        </div>
     )
 }
